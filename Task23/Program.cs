cbe934ecfd71aee9c8dc38bb576368dd7c35a7ee
Natal <nat_n_e@mail.ru>
2023-04-04 16:18:42 +0300

    Added program for Task23

--- Task23/Program.cs
+++ Task23/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 23: Напишите программу, которая принимает на вход
+// число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+// Заготовка!!!
+
+void CubesOfNumbers(int num) // int num = number
+{
+for (int i = 1; i <= num; i++)
+{
+    int cube = i*i*i;
+    Console.Write($"{cube}  ");
+}
+Console.WriteLine();
+}
+
+Console.Write("Input natural number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+while (number<1)
+{
+    Console.WriteLine("Your input is uncorrect, please, input naturat number: ");
+    number = Convert.ToInt32(Console.ReadLine());
+}
+CubesOfNumbers(number);
