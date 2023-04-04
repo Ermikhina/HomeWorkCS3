30878bbe9ec7ce9a9ddcdc0ce11443b7903862d5
Natal <nat_n_e@mail.ru>
2023-04-04 16:02:00 +0300

    Added program for task19

---
+++ Task19/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число 
+// и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+string Palind(int num)
+{
+    int dig1 = num / 10000;
+    int dig2 = num / 1000 % 10;
+    int dig4 = num / 10 % 10;
+    int dig5 = num % 10;
+    string result = (dig1 == dig5 && dig2 == dig4) ? "palindrome" : "not palindrome";
+    return result;
+}
+Console.Write("Input 5-digit number: ");
+int number = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+while (number < 10000 || number > 99999)
+{
+    Console.WriteLine("Your input is uncorrect, please, input 5-digit number: ");
+    number = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+}
+string output = Palind(number);
+Console.Write($"Number {number} is {output}");
